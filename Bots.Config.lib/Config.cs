d5bc901ebff465fd9c6e309c04d9799a6b26d52a
Zheka@RAUSHWEB <rausch08@yandex.ru>
2023-03-06 15:54:40 +0300

    Запускаем бота

---
+++ Bots.Config.lib/Config.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bots.Config.lib
+{
+    /// <summary>
+    /// Конфигурационный файл и класс.
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        /// Приватнвй ключ.
+        /// </summary>
+        public string key { get; }
+
+        /// <summary>
+        /// Публичная переменная.
+        /// </summary>
+        public string AccessToken;
+
+        public Config()
+        {
+            key = "6239153127:AAGik2jc-_UDxgTpE-ORtoTP3Ffa6dy2ujM";
+            switch (AccessToken)
+            {
+                case (null):
+                    AccessToken = key;
+                    break;
+                case (""):
+                    AccessToken = key;
+                    break;
+                case (" "):
+                    AccessToken = key;
+                    break;
+            }
+        }
+    }
+}
--- Telegram.Bot.CMD/Program.cs
+++ Telegram.Bot.CMD/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Bots.Config.lib;
+using Telegram.Bot;
 
 namespace Telegram.Bot.CMD
 {
     class Program
     {
-        static void Main(string[] args)
+        private static TelegramBotClient bot;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Config config = new();
+            Console.WriteLine("Привет мир!");
+            bot = new(config.key);
+
+            var me = await bot.GetMeAsync();
+            Console.WriteLine($"Мой ид: {me.Id} меня зовут {me.FirstName}.");
+
+            Console.ReadKey();
         }
     }
 }
